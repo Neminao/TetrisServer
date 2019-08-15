5d25868464176bcb0cedb2557ec180691be4024f
Neminao <nemanja.nedeljkovic@mail.com>
2019-08-15 13:13:39 +0200

    ClearDB

--- database/MySQLConnection.js
+++ database/MySQLConnection.js
@@ -1,18 +1,34 @@
 "use strict";
 exports.__esModule = true;
 var mysql = require("mysql");
-const con = mysql.createConnection({
+const config = {
     host: "eu-cdbr-west-02.cleardb.net",
     user: "bbbe4c4b9258de",
     password: "6cd2ff07",
     database: "heroku_22978428292f607"
-});
-con.connect(function (err) {
-    if (err)
-        throw err;
-    console.log("Connected");
-});
+};
+let con = mysql.createConnection(config);
+function handleDisconnect(connection){
+    connection.on('error', function(err) {
+        if(!err.fatal){
+            return;
+        }
 
+        if(err.code !== 'PROTOCOL_CONNECTION_LOST') {
+            throw err;
+        }
+
+        con = mysql.createConnection(config);
+        handleDisconnect(con);
+        con.connect(function (err) {
+            if (err)
+                throw err;
+            console.log("Connected");
+        });
+    })
+}
+
+handleDisconnect(con);
 
 
 module.exports = {
